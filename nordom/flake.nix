509c4c5a98171571c4c3d0b49885f3204c1c6804
Hendrik Moens <hendrik@moens.io>
2026-01-14 14:39:56 +0100

    add nordom config

---
+++ nordom/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "NixOS configuration for nixos desktop";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+}
