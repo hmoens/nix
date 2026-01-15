fbd7f80273044f2125bb71c0c1a955e2e328255f
Hendrik Moens <hendrik@moens.io>
2026-01-15 19:02:21 +0100

    update to 25.11

--- nordom/flake.nix
+++ nordom/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration for nixos desktop";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
   outputs =
